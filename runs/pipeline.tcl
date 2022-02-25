a1e3b19cca39131eb5fb7ace8558a77ff390f5d5
Michael Ekstrand <michaelekstrand@boisestate.edu>
2022-02-25 15:20:19 -0700

    environment stuff

--- runs/pipeline.tcl
+++ runs/pipeline.tcl
@@ -8,42 +8,10 @@
 #     tclsh runs/pipeline.tcl
 
 source runs/dvcgen.tcl
-
-set excludes [list]
-
-proc key {ds algo} {
-    return "$ds-$algo"
-}
-
-proc exclude args {
-    set ver all
-    set ds all
-    # reference: https://stackoverflow.com/a/31112023/1385039
-    while {[llength $args]} {
-        switch -- [lindex $args 0] {
-            -v {set args [lassign $args - ver]}
-            -d {set args [lassign $args - ds]}
-            -* {error "unknown option [lindex $args 0]"}
-            default break
-        }
-    }
-    set algo [lindex $args 0]
-
-    lappend ::excludes "$ver/$ds-$algo"
-}
-
-proc allowed {ver ds algo} {
-    set queries [list "$ver/$ds-$algo" "all/$ds-$algo" "$ver/all-$algo" "all/all-$algo"]
-    foreach q $queries {
-        if {[expr [lsearch $::excludes $q] >= 0]} {
-            return 0
-        }
-    }
-    return 1
-}
+source runs/pipe-utils.tcl
 
 set versions {
-    0.9 0.10 0.11 0.12 0.13
+    0.9 0.10 0.11 0.12 0.13 0.14
     main
 }
 
