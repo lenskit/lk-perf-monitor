5a52a325e6e5fe587ecf863bf615f3467ab1f63e
Michael Ekstrand <mdekstrand@drexel.edu>
2023-11-03 22:35:30 +0000

    invoke env

--- tasks.py
+++ tasks.py
@@ -1,5 +1,5 @@
 import sys
-import os
+from os import fspath
 from pathlib import Path
 import json
 
@@ -7,6 +7,13 @@ from ruamel.yaml import YAML
 
 from _jsonnet import evaluate_file
 from invoke import task
+
+ENV_BASE = Path('envs')
+
+
+def _msg(fmt, *args):
+    print(fmt.format(*args), file=sys.stderr)
+
 
 @task
 def update_pipeline(c):
@@ -21,8 +28,31 @@ def update_pipeline(c):
     pipes = runs.glob('*/dvc.jsonnet')
     for file in pipes:
         print('processing', file, file=sys.stderr)
-        config = evaluate_file(os.fspath(file))
+        config = evaluate_file(fspath(file))
         config = json.loads(config)
         yf = file.with_suffix('.yaml')
         with yf.open('w', encoding='utf8') as f:
             yaml.dump(config, f)
+
+
+@task
+def create_env(c, version=None, replace=False):
+    """
+    Create the environment for a LensKit version.
+    """
+
+    if version is not None:
+        versions = [version]
+    else:
+        versions = [ef.stem[3:] for ef in ENV_BASE.glob('lk-*.yml')]
+    
+    for ver in versions:
+        env_file = ENV_BASE / f'lk-{ver}.yml'
+        env_dir = ENV_BASE / f'lk-{ver}-env'
+
+        _msg('creating LensKit environment for {}', ver)
+        _msg('environment file: {}', env_file)
+        if replace:
+            c.run(f'mamba env create --force -p {fspath(env_dir)} -f {fspath(env_file)}', echo=True)
+        else:
+            c.run(f'mamba env update -p {fspath(env_dir)} -f {fspath(env_file)}', echo=True)
